7b0465db815a90e5f66fe3382def0cd11e3aadad
Quiarom <userquiarom@gmail.com>
2022-04-10 08:09:09 -0400

    Add Ansible Download Script and Ansible Tasks

---
+++ ansible-run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt-get update -y
+sudo apt-get install -y curl git software-properties-common ansible
+
+## Pull Ansible
+
+# sudo ansible-pull -U *Insert repo Dotfile* 
